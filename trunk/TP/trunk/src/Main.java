a427a6cabe9e3bd039ebd622cbdce99c204d8f17
aabize <aabize@2edc24d2-cef8-467f-d11b-342eeb0b93b2>
2011-04-30 16:51:12 +0000

    Main de prueba de la estrategia

--- trunk/TP/trunk/src/Main.java
+++ trunk/TP/trunk/src/Main.java
@@ -1,13 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import juego.Constantes;
+import juego.EstimadorGradosLibertad;
+import juego.Estrategia;
+import juego.EstrategiaAlfaBeta;
+import juego.Posicion;
+import juego.Tablero;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GO!");
 
+		Tablero tablero = new Tablero(7);
+		Estrategia estrategia = new EstrategiaAlfaBeta(new EstimadorGradosLibertad(), 5, 5);
+		
+		while (true) {
+			Posicion jugada = estrategia.obtenerMejorJugada(tablero);
+			System.out.println("x: " + jugada.getX() + " y: " + jugada.getY());
+			tablero = tablero.agregarJugada(jugada);
+			
+		    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		    String x = in.readLine();
+		    String y = in.readLine();
+		    Posicion jugadaOponente = new Posicion(Integer.parseInt(x), Integer.parseInt(y), Constantes.BLANCO);
+		    tablero = tablero.agregarJugada(jugadaOponente);
+		}
 	}
 
 }
